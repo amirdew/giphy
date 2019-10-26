c2ad5bf1e1a8fc282dcbc60fd7e5a28b19ec15bb
Amir Khorsandi <khorsandi@me.com>
2019-10-26 11:48:54 +0200

    Initial Commit

---
+++ Picnic GiphyTests/Picnic_GiphyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Picnic_GiphyTests.swift
+//  Picnic GiphyTests
+//
+//  Created by Amir on 26/10/2019.
+//  Copyright © 2019 Amir. All rights reserved.
+//
+
+import XCTest
+@testable import Picnic_Giphy
+
+class Picnic_GiphyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
